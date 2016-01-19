80d96d29d9aa99da89c96bc6487b2938f4fc03a9
Joseph Conan Montgomery <joseph.conan.montgomery@gmail.com>
2016-01-19 13:15:55 -0500

    Refactored ImageFetcher.

--- app/src/main/java/com/josephcmontgomery/bookscanner/Tools/ImageFetcher.java
+++ app/src/main/java/com/josephcmontgomery/bookscanner/Tools/ImageFetcher.java
@@ -2,16 +2,51 @@ package com.josephcmontgomery.bookscanner.Tools;
 
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.ImageView;
 
 import java.net.URL;
 import java.util.HashMap;
 
-/**
- * Created by Joseph on 1/10/2016.
- */
 public class ImageFetcher {
-    private static HashMap<String, Drawable> cache = new HashMap<String, Drawable>();
+    private static HashMap<String, Drawable> cache = new HashMap<>();
+
+    public static void loadImage(final String imageUrl, final ImageView image){
+        if(cache.containsKey(imageUrl)){
+            image.setImageDrawable(getDrawable(imageUrl));
+        }
+        else {
+            setImageFromUrl(imageUrl, image);
+        }
+    }
+
+    private static void setImageFromUrl(final String imageUrl, final ImageView image){
+        new AsyncTask<Void, Void, Void>() {
+            Drawable drawThumb;
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    URL urlThumb = new URL(imageUrl);
+                    drawThumb = Drawable.createFromStream(urlThumb.openStream(), "src");
+                } catch (Exception e) {
+                    if(e.getMessage() != null) {
+                        Log.d("EXCEPTION", e.getMessage());
+                    }
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                if (drawThumb != null) {
+                    ImageFetcher.insertDrawable(imageUrl, drawThumb);
+                    image.setImageDrawable(drawThumb);
+                }
+            }
+
+        }.execute();
+    }
 
     private static void insertDrawable(String imageUrl, Drawable image){
         cache.put(imageUrl, image);
@@ -19,36 +54,5 @@ public class ImageFetcher {
 
     private static Drawable getDrawable(String imageUrl){
         return cache.get(imageUrl);
-    }
-
-    public static void loadImage(final String imageUrl, final ImageView image){
-        if(cache.containsKey(imageUrl)){
-            image.setImageDrawable(getDrawable(imageUrl));
-        }
-        else {
-            new AsyncTask<Void, Void, Drawable>() {
-                Drawable thumb_d;
-
-                @Override
-                protected Drawable doInBackground(Void... params) {
-                    try {
-                        URL thumb_u = new URL(imageUrl);
-                        thumb_d = Drawable.createFromStream(thumb_u.openStream(), "src");
-                    } catch (Exception e) {
-                        // log error
-                    }
-                    return thumb_d;
-                }
-
-                @Override
-                protected void onPostExecute(Drawable result) {
-                    if (thumb_d != null) {
-                        ImageFetcher.insertDrawable(imageUrl, thumb_d);
-                        image.setImageDrawable(thumb_d);
-                    }
-                }
-
-            }.execute();
-        }
     }
 }
