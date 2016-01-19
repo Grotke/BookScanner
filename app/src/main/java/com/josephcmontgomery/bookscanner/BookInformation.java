6e55f88dbe085df82375069273f680b095c8c285
Joseph Conan Montgomery <joseph.conan.montgomery@gmail.com>
2016-01-19 11:43:56 -0500

    Refactored BookInformation.

--- app/src/main/java/com/josephcmontgomery/bookscanner/BookInformation.java
+++ app/src/main/java/com/josephcmontgomery/bookscanner/BookInformation.java
@@ -1,11 +1,7 @@
 package com.josephcmontgomery.bookscanner;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
-/**
- * Created by Joseph on 12/15/2015.
- */
 public class BookInformation implements Serializable{
     public String title;
     public String subtitle;
@@ -33,23 +29,14 @@ public class BookInformation implements Serializable{
 
     @Override
     public String toString(){
-        return "ISBN: " + isbn + "\nTITLE: " + title + "\nSUBTITLE: " + subtitle + "\nDESCRIPTION: " + description + "\nPAGE COUNT: "+ pageCount+ "\nAVERAGE RATING: " + averageRating + "\nRATINGS COUNT: " + ratingsCount + "\nLOCATION: " + location + "\nTIME LAST UPDATED: " + timeLastUpdated;
+        return "ISBN: " + isbn +
+                "\nTITLE: " + title +
+                "\nSUBTITLE: " + subtitle +
+                "\nDESCRIPTION: " + description +
+                "\nPAGE COUNT: "+ pageCount+
+                "\nAVERAGE RATING: " + averageRating +
+                "\nRATINGS COUNT: " + ratingsCount +
+                "\nLOCATION: " + location +
+                "\nTIME LAST UPDATED: " + timeLastUpdated;
     }
-
-    private String convertListToString(ArrayList<String> list){
-        String listString = "";
-
-        for (String s : list)
-        {
-            if(listString.equals("")) {
-                listString += s;
-            }
-            else{
-                listString += ", " + s;
-            }
-        }
-        return listString;
-    }
-
-
 }
